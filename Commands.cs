3b209c480d332dbafcb51e9618e61855b8ace023
Charles Ayeni <charles.work20@gmail.com>
2022-08-19 14:04:46 -0400

    Add project files.

---
+++ Commands.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace RandoDiscordBot
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong!");
+        }
+        [Command("daddy")]
+        public async Task Daddy()
+        {
+            await ReplyAsync("I was a random bot that my handsome daddy C9Flexer decided to make when he was bored.\nMy only goal in life is to troll beastie!");
+        }
+        [Command("hardR")]
+        public async Task HardR()
+        {
+            await ReplyAsync("Im about to drop the hard R!");
+        }
+        [Command("n")]
+        public async Task n()
+        {
+            await ReplyAsync("i\ng\ng");
+        }
+
+        [Command("e")]
+        public async Task e()
+        {
+            await ReplyAsync("r");
+        }
+
+        [Command("abort")]
+        public async Task abort()
+        {
+            await ReplyAsync("Fetus deletus");
+        }
+
+        [Command("meme")]
+        public async Task meme()
+        {
+            var embed = new EmbedBuilder();
+
+            embed.WithImageUrl("https://static.wikia.nocookie.net/runescape2/images/5/56/Frog_%28NPC%29.png/revision/latest?cb=20160531202106");
+
+            await ReplyAsync("", false, embed.Build());
+        }
+        [Command("ni")]
+        public async Task ni()
+        {
+            await ReplyAsync("gger");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RandoDiscordBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        static void Main(string[] args) => new Program().RunBotAysnc().GetAwaiter().GetResult();
+
+        public async Task RunBotAysnc()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            //singletone ensure only one bot is running at a time
+            _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
+
+            string token = "MTAxMDIyNjYwMDU5MzMzODQ1MQ.G-IyRU.JYQBMc5hr7EI-saPztj29-oGemhFP-E3u3vqcY";
+            _client.Log += _client_Log;
+            
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+
+
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync(){
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage args)
+        {
+            var message = args as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            string[] filters = { "ni" };
+            string content = message.Content;
+            bool contains = filters.Any(x => content.Split(' ').Any(y => y.Contains(x)));
+
+            int argPos = 0;
+            if (message.HasStringPrefix("_", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+            //message without prefix
+            else if (contains)
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                return;
+            }
+        }
+    }
+}
